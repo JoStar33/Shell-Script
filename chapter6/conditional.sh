27c72a55b1bff80d0228058127b19cf56535f12c
HoCG <rhwkd5012@naver.com>
2024-05-02 23:58:36 +0900

    update: 연습추가

--- chapter6/conditional.sh
+++ chapter6/conditional.sh
@@ -7,3 +7,9 @@ fi
 
 # if를 쓰고 []안에 식을 쓴다음 then으로 실행할 코드를 작성한다.
 # 마지막으로 fi로 if를 닫아주면 된다.
+
+
+VALUE2=3
+if [ `expr $VALUE + $VALUE2` -ge 13 ] ; then
+  echo "The Number is greater than or Equal 13"
+fi
